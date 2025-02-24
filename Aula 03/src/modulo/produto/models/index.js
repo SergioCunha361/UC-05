416ccf93eb3732c40956c64062a86a4d91f36d2b
Sergio Cunha Lima Bezerra <sergiocunha50@gmail.com>
2025-02-24 17:33:59 -0300

    aula bcackend

---
+++ Aula 03/src/modulo/produto/models/index.js
@@ -0,0 +1,11 @@
+const pool = require('../../../config/database');
+function listarProdutos() {
+    try {
+        const query = 'select * from produtos'
+        const resultado = pool.query(query)
+        if (!resultado) {
+            return { mensagem: "Banco de dados vazio" }
+        }
+    } catch (error) {
+        
+    }
