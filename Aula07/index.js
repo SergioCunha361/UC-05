80463647c1120be271c7bf2b14fd1c7e920aaecb
SergioCunha361 <sergiocunha50@gmail.com>
2025-03-17 12:59:57 -0300

    autalizado em casa dia 17-03

--- Aula07/index.js
+++ Aula07/index.js
@@ -14,120 +14,120 @@ app.use(express.json());
 
 
 
-app.get('/produtos', async (requisicao, resposta) => {
+app.get('/alunos', async (requisicao, resposta) => {
   try {
-  const consulta = `select * from produto`
-    const produtos = await pool.query(consulta)
-    if (produtos.rows.length === 0) {
+  const consulta = `select * from aluno`
+    const alunos = await pool.query(consulta)
+    if (alunos.rows.length === 0) {
         return resposta.status(200).json({mensagem:"Banco de dados Vazio"})
     }
-    return resposta.status(200).json(produtos.rows);
+    return resposta.status(200).json(alunos.rows);
   } catch (error) {
-  resposta.status(500).json({ mensagem:"Erro ao buscar produto", erro:error.mensagem})
+  resposta.status(500).json({ mensagem:"Erro ao buscar aluno", erro:error.mensagem})
   }
 });
 
-app.post('/produtos', async (requisicao, resposta) => {
+app.post('/alunos', async (requisicao, resposta) => {
   try {
-  const { nome, preco, quantidade } = requisicao.body;
-  if (!nome || !preco || !quantidade) {
+  const { matricula, nome, email, senha } = requisicao.body;
+  if (!matricula || !nome || !email || !senha) {
     return resposta.status(200).json({mensagem:"Todos os dados devem ser preenchidos"});
   }
-  const dados = [ nome, preco, quantidade ];//a funcao query necesita que os dados sejam passados em array
-  const consulta = `insert into produto (nome, preco, quantidade) values ($1, $2, $3) returning *`;
-  // o returnig * é para retornar o produto criado
+  const dados = [ matricula, nome, email, senha ];//a funcao query necesita que os dados sejam passados em array
+  const consulta = `insert into aluno (matricula, nome, email, senha) values ($1, $2, $3, $4) returning *`;
+  // o returnig * é para retornar o aluno criado
   await pool.query(consulta, dados); //necessita que os dados sejam passados em array
   //O banco de dados está sendo criado aqui no query
   // poderia ser assim:
-  //await pool.query(insert into produto (nome, preco, quantidade) values ($1, $2, $3) returning *,[ nome, preco, quantidade ] );
-  resposta.status(201).json({ mensagem: "Produto criado com sucesso"});// não tem return porque não tem IF
+  //await pool.query(insert into produto (matricula, nome, email, senha ) values ($1, $2, $3, $4) returning *,[ matricula, nome, email, senha  ] );
+  //  por issi cria-se a variavel dados
+  resposta.status(201).json({ mensagem: "Aluno cadastrado com sucesso"});// não tem return porque não tem IF
     } catch (error){
-        resposta.status(500).json({mensagem:"Erro ao criar produto",erro: error.message})
+        resposta.status(500).json({mensagem:"Erro ao cadastrar aluno",erro: error.message})
     }
 });
 
 
-app.put('/produtos/:id', async (requisicao, resposta) => {
+app.put('/alunos/:id', async (requisicao, resposta) => {
   try {
     // loacalhost:3000/produtos/1 - O 1 é o parametro
 const id  = requisicao.params.id;
-const { novoNome, novoPreco, novaQuantidade } = requisicao.body //levando dados para requisição
+const { novoMatricula, novoNome, novoEmail, novoSenha  } = requisicao.body //levando dados para requisição
 
 if (!id){
   return resposta.status(404).json({mensagem:"Informe o parametro"})
 }
 const dados1 = [id];// id em formato de array para a consulta
-const consulta1 = `select * from produto where id = $1`
+const consulta1 = `select * from aluno where id = $1`
 const resultado1 = await pool.query(consulta1, dados1)
 //resutlado1 grava todos os elemtos que foram encontrados na consulta1
 if (!resultado1.rows.length === 0){
-  return resposta.status(404).json({mensagem: "Produto não enconatrado"})
+  return resposta.status(404).json({mensagem: "Aluno não enconatrado"})
   }
-const dados2 =  [id, novoNome, novoPreco, novaQuantidade]
-const consulta2 = `update produto set nome = $2, preco = $3, quantidade = $4 where id= $1 returning *`
+const dados2 =  [id, novoMatricula, novoNome, novoEmail, novoSenha]
+const consulta2 = `update aluno set matricula = $2, nome = $3, email = $4, senha = $5 where id= $1 returning *`
 await pool.query(consulta2, dados2)
-resposta.status(200).json({mensagem: "Produto atualizado com sucesso"})
+resposta.status(200).json({mensagem: "Aluno atualizado com sucesso"})
 
 } catch (error){
-      resposta.status(500).json({mensagem:"Erro ao editar produto",erro: error.message})
+      resposta.status(500).json({mensagem:"Erro ao editar Aluno",erro: error.message})
 }
 })
 
 
-app.delete('/produtos/:id', async (requisicao, resposta) => {
-  try {
+app.delete('/alunos/:id', async (requisicao, resposta) => {
+try {
     // loacalhost:3000/produtos/1 - O 1 é o parametro
     const  id  = requisicao.params.id;
-
-const dados1 = [id];
-const consulta1 = `select * from produto where id = $1`
-const resultado1 = await pool.query(consulta1, dados1)
+    const dados1 = [id];
+    const consulta1 = `select * from aluno where id = $1`
+    const resultado1 = await pool.query(consulta1, dados1)
  //resultado1 armazena linha pesquisada
-if (!id){
-  return resposta.status(404).json({mensagem:"Informe o parametro"}) 
+   if (!id){
+     return resposta.status(404).json({mensagem:"Informe o parametro"}) 
 }
-if (!resultado1.rows.length === 0){
-  return resposta.status(404).json({mensagem: "Produto não enconatrado"})
+  if (!resultado1.rows.length === 0){
+     return resposta.status(404).json({mensagem: "Aluno não enconatrado"})
 }
 const dados2 = [id];
-const consulta2 = `delete from produto where id = $1`
+const consulta2 = `delete from aluno where id = $1`
 await pool.query(consulta2, dados2)
-resposta.status(200).json({mensagem:"Produto deletado com sucesso"})
+resposta.status(200).json({mensagem:"Aluno deletado com sucesso"})
 
 } catch (error){
-    resposta.status(500).json({mensagem:"Erro ao excluir produto",erro: error.message})
+    resposta.status(500).json({mensagem:"Erro ao excluir aluno",erro: error.message})
 }
 });
 
 
 //Buscar produto por id
-app.get("/produtos/:id", async (requisicao, resposta) => {
+app.get("/alunoss/:id", async (requisicao, resposta) => {
   try {
     const id = requisicao.params.id;  // esse id é string naturalmente
     const dados1 = [id];
-    const consulta1 = `select * from produto where id = $1`;
+    const consulta1 = `select * from aluno where id = $1`;
     const resultado1 = await pool.query(consulta1, dados1);
 
     if (resultado1.rows.length === 0) {
-      return resposta.status(404).json({ mensagem: "Produto não encontrado" });      
+      return resposta.status(404).json({ mensagem: "Aluno não encontrado" });      
     }
     resposta.status(200).json(resultado1.rows[0]);
    
-  } catch (error) {resposta.status(500).json({mensagem: "Erro ao buscar produto", erro: error.message})    
+  } catch (error) {resposta.status(500).json({mensagem: "Erro ao buscar aluno", erro: error.message})    
   }
 })
 
 
 //Deletar todos os produtos
-app.delete("/produtos", async (requisicao, resposta) => {
+app.delete("/alunos", async (requisicao, resposta) => {
   try {
     
-    const consulta = `delete from produto`;
+    const consulta = `delete from aluno`;
     await pool.query(consulta);  
-    resposta.status(200).json({mensagem: "Todos os produtos foram dletados"})
+    resposta.status(200).json({mensagem: "Todos os alunos foram dletados"})
 
   } catch (error) {
-    resposta.status(500).json({mensagem:"Erro ao deletar produtos", erro: error.message})  
+    resposta.status(500).json({mensagem:"Erro ao deletar alunos", erro: error.message})  
   }
 })
 
--- Aula07/src/modules/aluno/controllers/index.js
+++ Aula07/src/modules/aluno/controllers/index.js
@@ -4,7 +4,7 @@ const AlunoModel = require ('../models/index')
         static async criar(requisicao, resposta){
                 try {
                         const {matricula, nome, email,senha} = requisicao.body
-                        if (!matricula || !nome || email !! senha){
+                        if (!matricula || !nome || !email || !senha){
                             return resposta.status(400).json({mensagem:"Todos os campos devem ser preenchido"})
                         }
                         const novoAluno = await AlunoModel.criar(matricula, nome, email, senha) 
@@ -14,7 +14,7 @@ const AlunoModel = require ('../models/index')
                 }
         }
         static editar(requisicao, resposta){
-
+            
         }
         static async listarTodos(requisicao, resposta){
                 
@@ -23,16 +23,15 @@ const AlunoModel = require ('../models/index')
         static async listarPorMatricula(requisicao, resposta){
             try {
                 const id = requisicao.params.id
-                const aluno = await AlunoModel.listarPorId(matriucula)
+                const aluno = await AlunoModel.listarPorId(matricula)
                 if (!aluno){
-                    return resposta.status(400).json({mensagem:"Aluno não encontrado"})
+                    return resposta.status(400).json({mensagem: "Aluno não encontrado"})
                 }
                 resposta.status(200).json(aluno)
-            catch (error) {
+            }catch (error) {
                     resposta.status(500).json({mensagem: "Erro ao criar o aluno", erro: error.mensage})   
             }    
                 
-            }
 
         }
         static excluirPorMatriula(requisicao, resposta){
