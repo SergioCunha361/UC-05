a20b8bed404aa5ab1c700c5b11adc5c680f2fbe7
Valtemir Procopio <i3831@rn.senac.local>
2025-03-24 17:18:23 -0300

    feat: adiciona módulo de aluno com rotas, controladores, modelos e configuração do banco de dados

---
+++ Aula 10/index.js
@@ -0,0 +1,18 @@
+// Importando com (commonjs)
+const express = require("express")
+const debug = require('debug')('app');
+const dotenv = require("dotenv");
+const router = require('./src/modules/aluno/routes/index')
+dotenv.config();
+
+const port = process.env.PORTA;
+const app = express();
+
+// Aplicação use express como json(javascript object notation)
+app.use(express.json());
+
+app.use(router)
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
