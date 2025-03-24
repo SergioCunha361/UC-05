302cf5949f1977158a4f769a4b835d1c90c69f86
SERGIO CUNHA LIMA BEZERRA <a92345826@rn.senac.local>
2025-03-24 17:19:07 -0300

    add:aula 24-03

--- Medicamentos/src/config/database.js
+++ Medicamentos/src/config/database.js
@@ -11,3 +11,12 @@ const pool = new Pool({
 });
 
 module.exports = { pool };
+// CREATE TABLE IF NOT EXISTS pedido (
+//     id SERIAL PRIMARY KEY,
+//     cliente VARCHAR(60) NOT NULL,
+//     medicamentos TEXT NOT NULL,
+//     quantidade INT NOT NULL,
+//     valor_total DECIMAL(10, 2) NOT NULL,
+//     status VARCHAR(5) NOT NULL,
+//     forma_pagamento VARCHAR(30) NOT NULL
+// );
