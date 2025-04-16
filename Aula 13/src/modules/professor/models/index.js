35649223dd183c08b44649944419b5cda2eeb681
Valtemir Procopio <i3831@rn.senac.local>
2025-04-16 14:47:53 -0300

    feat: adiciona modelo de Professor com validações para matrícula, nome, e-mail e senha

--- Aula 13/src/modules/professor/models/index.js
+++ Aula 13/src/modules/professor/models/index.js
@@ -1,1 +1,43 @@
-// Se você tá lendo, é porque eu pedi para fazer
+const { DataTypes } = require('sequelize')
+const sequelize = require('../../../config/configDb')
+
+const Professor = sequelize.define('Professor', {
+    matricula: {
+        type: DataTypes.CHAR(8),
+        primaryKey: true,
+        validate:{
+            is:{
+                args: /^[A-Za-z][0-9]{7}$/,
+                msg:'A matricula deve começar com uma letra e ter mais 7 números!'
+            }
+        }
+    },
+    nome: {
+        type: DataTypes.STRING(100),
+        allowNull:false,
+        validate:{
+            len:{
+                args:[100]
+            }
+        }
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate:{
+            isEmail:{
+                msg:'Forneça um e-mail valido!'
+            }
+        }
+    },
+    senha: {
+        type: DataTypes.CHAR(10),
+        allowNull: false,
+        validate:{
+            args: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{10}$/,
+            msg: 'A senha deve ter exatamente 10 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.'
+        }
+    }
+})
+module.exports = Professor
